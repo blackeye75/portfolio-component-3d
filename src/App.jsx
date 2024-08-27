d32ad8c0e782e528e80223afca008f1d8ea342c6
blackeye75 <priyanshuraj22275@gmail.com>
2024-08-28 01:22:33 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Canvas } from "@react-three/fiber";
+import "./style.css"
 function App() {
-  return <Canvas></Canvas>;
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial />
+      </mesh>
+    </Canvas>
+  );
 }
 
 export default App;
