3c0e546cd0c48ccbc4509f884cfffdbb9b77bde4
blackeye75 <priyanshuraj22275@gmail.com>
2024-08-30 01:31:08 +0530

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,22 @@ function App() {
   return (
     <>
       <Canvas flat camera={{ fov: 35 }}>
-        {/* <OrbitControls /> */}
+        <OrbitControls />
         <ambientLight />
         <Scene />
         <EffectComposer>
           <Bloom
             intensity={7.0} // The bloom intensity.
-            luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+            luminanceThreshold={0.01} // luminance threshold. Raise this value to mask out darker elements in the scene.
             luminanceSmoothing={0.9} // smoothness of the luminance threshold. Range is [0, 1]
             mipmapBlur
           />
           {/* <ToneMapping adaptive /> */}
         </EffectComposer>
       </Canvas>
-      <h1 className=" text font-bold text-9xl text-center ">Protfolio component</h1>
+      <h1 className=" text font-bold text-9xl text-center ">
+        Protfolio component
+      </h1>
     </>
   );
 }
