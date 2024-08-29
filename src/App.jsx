49a7faf2de2940421e1391f1f43fd4261385cef4
blackeye75 <priyanshuraj22275@gmail.com>
2024-08-29 19:02:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import "./style.css";
 import { OrbitControls } from "@react-three/drei";
 
 import { Scene } from "./Scene";
+import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
 function App() {
-  
   return (
-    <Canvas camera={{fov:35}}>
-      <OrbitControls />
+    <>
+    
+    <Canvas flat camera={{ fov: 35 }}>
+      {/* <OrbitControls /> */}
       <ambientLight />
-     <Scene/>
+      <Scene />
+      <EffectComposer>
+        <Bloom
+          intensity={7.0} // The bloom intensity.
+          luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.9} // smoothness of the luminance threshold. Range is [0, 1]
+          mipmapBlur
+        />
+        {/* <ToneMapping adaptive /> */}
+      </EffectComposer>
     </Canvas>
+    </>
   );
 }
 
--- src/Scene.jsx
+++ src/Scene.jsx
@@ -5,14 +5,16 @@ import { useFrame } from "@react-three/fiber";
 
 export const Scene = () => {
   let tex = useTexture("./image.png");
- let cyl= useRef(null)
-  useFrame((state,delta)=>{
-    cyl.current.rotation.y+=delta;
-  })
+  let cyl = useRef(null);
+  useFrame((state, delta) => {
+    cyl.current.rotation.y += delta;
+  });
   return (
-    <mesh ref={cyl} rotation={[0, 1.4, 0.5]}>
-      <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
-      <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
-    </mesh>
+    <group rotation={[0, 1.4, 0.5]}npm  >
+      <mesh ref={cyl} >
+        <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+        <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
+      </mesh>
+    </group>
   );
 };
